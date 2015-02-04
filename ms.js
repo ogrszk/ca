47c9467c2b9553694c5791a705c1f3ad6be874b1
ogrszk <ogrszk@gmail.com>
2015-02-05 01:03:18 +0900

    maked mine

--- ms.js
+++ ms.js
@@ -16,12 +16,70 @@ for(i=0;i<f_n;i++){
 		m_st[i][j] = false;
 	}
 }
+var r = [];
+for(i=0;i<f_n*f_n;i++){
+	r[i] = i;
+}
+for(i=0;i<m_n;i++){
+	var t = Math.floor(Math.random()*f_n*f_n);
+	console.log(t);
+	var tmp = r[i];
+	r[i] = r[t];
+	r[t] = tmp;
+}
+console.log(r);
+var ttt;
+for(i=0;i<m_n;i++){
+	m_st[r[i]%f_n][Math.floor(r[i]/f_n)] = true;
+	ttt = (r[i]%f_n)+","+(Math.floor(r[i]/f_n));
+	console.log(ttt);
+}
+var tt = "";
+for(y=0;y<f_n;y++){
+	for(x=0;x<f_n;x++){
+		tt += m_st[x][y] + ",";
+	}
+}
+console.log(tt);
+
+function refresh(){
+	for(y=0;y<f_n;y++){
+		for(x=0;x<f_n;x++){
+			var a = document.getElementById("x"+x+"y"+y);
+			a.classList.remove("shadow_tl","shadow_l","shadow_t");
+			if(p_st[x][y]){
+				if(x>0 && y>0 && !p_st[x][y-1] && !p_st[x-1][y]){
+					a.classList.add("shadow_tl");
+				}else if(x>0 && !p_st[x-1][y]){
+					a.classList.add("shadow_l");
+				}else if(y>0 && !p_st[x][y-1]){
+					a.classList.add("shadow_t");
+				}
+			}
+		}
+	}
+}
+
+function pushMine(){
+	for(y=0;y<f_n;y++){
+		for(x=0;x<f_n;x++){
+			if(m_st[x][y]){
+				var a = document.getElementById("x"+x+"y"+y);
+				a.classList.add("pushedMine");
+			}
+		}
+	}
+}
 
 function clickGrid(x,y) {
 //	alert(x+','+y);
+	p_st[x][y] = true;
+	if(m_st[x][y]) pushMine();
 	var a = document.getElementById("x"+x+"y"+y);
 	a.classList.add("pushed");
-	p_st[x][y] = true;
+	refresh();
+	
+	
 }
 
 tmp = document.createDocumentFragment();
